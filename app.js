1d69c9cb06f66ad84c66a2288529153fd05bb68c
hurtaditoo <al415507@uji.es>
2025-01-18 19:04:04 +0100

    Bonus: Iteration 7 | Create a 404 Route

--- app.js
+++ app.js
@@ -25,19 +25,23 @@ app.use(morgan("dev"));
 // Start defining your routes here:
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/home.html");
-})
+});
 
 app.get("/blog", (req, res) => {
     res.sendFile(__dirname + "/views/blog.html");
-})
+});
 
 app.get("/api/projects", (req, res) => {
     res.json(projects);
-})
+});
 
 app.get("/api/articles", (req, res) => {
     res.json(articles);
-})
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/views/not-found.html");
+});
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
